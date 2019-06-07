3aced234e7e183a9b350092ce459842353920558
Paul Hansen <paul.c.hansen@gmail.com>
2019-06-06 17:29:33 -0700

    changed version text in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 requirements = ["pytest", "scipy", "numpy"]
-version = "0.6"
+version = "0.6.0"
 
 setup(
     name="xraymaterials",
