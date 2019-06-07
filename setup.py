5fcaf0cdcfb325ec0145118888e62a3e1db12486
Paul Hansen <paul.c.hansen@gmail.com>
2019-06-06 18:04:09 -0700

    added some materials

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 requirements = ["pytest", "scipy", "numpy"]
-version = "0.6.0"
+version = "0.6.1"
 
 setup(
     name="xraymaterials",
